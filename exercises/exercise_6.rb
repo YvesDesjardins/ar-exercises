0647ab26435f0e5b50c92dd3d949543c3b8d91ae
Yves Desjardins <YvesDesjardins@gmail.com>
2019-03-19 14:44:53 -0700

    fix hourly rate for some employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -10,10 +10,10 @@ puts "----------"
 
 # Your code goes here ...
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
-@store1.employees.create(first_name: "George", last_name: "Clooney", hourly_rate: 60000)
+@store1.employees.create(first_name: "George", last_name: "Clooney", hourly_rate: 200)
 @store2.employees.create(first_name: "Billy", last_name: "Bob", hourly_rate: 60)
-@store2.employees.create(first_name: "Angela", last_name: "Merkel", hourly_rate: 60000)
-@store2.employees.create(first_name: "Elon", last_name: "Musk", hourly_rate: 60000)
+@store2.employees.create(first_name: "Angela", last_name: "Merkel", hourly_rate: 200)
+@store2.employees.create(first_name: "Elon", last_name: "Musk", hourly_rate: 200)
 
 puts "Store 1 has #{@store1.employees.count} employees"
 puts "Store 2 has #{@store2.employees.count} employees"
