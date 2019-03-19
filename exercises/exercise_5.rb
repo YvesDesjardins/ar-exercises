c533ca9142eb62d10c27b69b16f7895dbe2933dc
Yves Desjardins <YvesDesjardins@gmail.com>
2019-03-19 14:37:50 -0700

    finalize ex 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,6 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "Annual revenue for all stores: #{Store.sum(:annual_revenue)}"
+puts "Average annual revenue per store: #{Store.sum(:annual_revenue)/Store.all.count}"
+puts "Number of stores over $1M revenue: #{Store.where("annual_revenue > ?", 1000000).count}"
